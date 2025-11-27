e6c6c13c8bd318792e253faf5056e6af37a6b057
Patryk Tomasz Wolowicz <patryktomasz.wolowicz@juliusbaer.com>
2025-11-27 05:27:20 +0100

    test app on Docker

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+
+r = redis.Redis(host='redis', port=6379)
+
+import debugpy
+debugpy.listen(("0.0.0.0", 5678))
+#debugpy.wait_for_client()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World1234567890!!!"}
+
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"Number of hits": r.get("hits")}
